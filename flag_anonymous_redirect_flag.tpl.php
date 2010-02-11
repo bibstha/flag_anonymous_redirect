cde687e0d26ddbd535dc94c926c0cd882f340c66
Bibek Shrestha <bibekshrestha@gmail.com>
2010-02-11 14:23:09 +0545

    Migrating changes to github as of now, initial commit

---
+++ flag_anonymous_redirect_flag.tpl.php
@@ -0,0 +1,7 @@
+<?php
+?>
+<span class="flag-wrapper flag-<?php echo $flag_name_css; ?>">
+  <a href="<?php echo $link_href; ?>" title="<?php echo $link_title; ?>" class="<?php print $flag_classes ?>" rel="nofollow">
+    <?php echo $link_text; ?>
+  </a><span class="flag-throbber">&nbsp;</span>    
+</span>
